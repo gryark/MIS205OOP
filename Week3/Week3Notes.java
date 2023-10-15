ed8bc3e6cc82ac128de9781a0883b40a5af704a5
Güray Arık <guray.arik@assistt.com.tr>
2023-10-15 12:12:20 +0300

    Week 3 Java Codes

---
+++ Week3/Week3Notes.java
@@ -0,0 +1,68 @@
+class Week3Notes{
+  /***Variables (Primitives)
+
+   **Numbers Literals :
+   * byte, short, int, long
+
+   ** Numbers Floating Point :
+   * float, double
+
+   ** Char value:
+   * char letter='A";
+
+   ** String values:
+   * String name:"Güray ARIK";
+
+   ** Booelan values (true, false)
+   * boolean isActive=false;
+    ***/
+
+  /***Named Constants
+   final datatype CONSTANTNAME = value
+   final double PI = 3.14; // Declare a constant
+   ***/
+
+  /***.13 Augmented Assignment Operators
+   The operators +, -, *, /, and % can be combined with the assignment operator to form
+   augmented operators.
+   count = count + 1;
+   count += 1
+   ***/
+
+  /*** Increment and Decrement Operators
+   The increment operator (+ +) and decrement operator (- -) are for incrementing and
+   decrementing a variable by 1.
+   int i = 3, j = 3;
+   i++; // i becomes 4    j——; // j becomes
+   int i = 3, j = 3;
+   ++i; // i becomes 4    ——j; // j becomes
+   ***/
+
+  /***  Numeric Type Conversions
+   Floating-point numbers can be converted into integers using explicit casting
+   double d = 4.5;
+   int i = (int) d; // i becomes 4, but d is still 4.5
+   type casting
+   widening a type // Casting a type with a small range to a type with a larger range
+   narrowing a type // Casting a type with a large range to a type with a smaller range
+   ***/
+/****CHAPTER 3
+ **boolean data type
+ * if statements
+ * if else statements
+ * if else if nested statements
+ * ternary operator
+ * switch case statements
+ ***/
+/****CHAPTER 4 Strings
+ *** Strings
+ * string methods
+ */
+
+/****CHAPTER 5 Loops
+ * while loops
+ * do-while loops
+ * for loops
+  */
+
+}
